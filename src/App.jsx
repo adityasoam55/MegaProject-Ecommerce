54ca810b80813eab6341dddac2de8ed8edc716b3
Aditya som <ADITYASOAM55@GMAIL.COM>
2024-06-15 12:08:35 +0530

    Site Name in Navbar changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import ProductListPage from './ProductListPage';
 import { Routes, Route } from 'react-router-dom';
 import ProductDetails from './ProductDetails';
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function NavBar({ productCount }) {
     return (
         <div className='flex justify-between items-center text-center bg-tomato-default w-full py-2 px-4'>
-            <h2>AMAZON.ORG</h2>
+            <h2 className='font-semibold'>MyEasyCart</h2>
             <div className='w-full flex justify-center gap-5 font-medium max-md:text-sm'>
                 <Link  className="hover:underline underline-offset-8" to="/">Home</Link>
                 <span>Categoery</span>
